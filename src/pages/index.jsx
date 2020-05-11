d0b19141c2cf48d1e78756003b67d798c42a4ee0
Rossana Bermudez <rossana.bermudez.de.la.hoz@pwc.com>
2020-05-11 17:44:58 +0200

    Scroll-to-top is gone now

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,21 +3,6 @@ import {Contact, Experience, Intro, Skills} from 'components/landing'
 import React from 'react'
 import 'styles/main.scss'
 
-let fabButton = document.getElementById('to-top')
-window.onscroll = () => {
-  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
-  if (fabButton) {
-    let y = window.scrollY
-    if (y > 0) {
-      fabButton.classList.remove('hidden')
-    } else {
-      fabButton.classList.add('hidden')
-    }
-  } else {
-    fabButton = document.getElementById('to-top')
-  }
-}
-
 export default () => (
   <Layout>
     <ScrollToTop />
