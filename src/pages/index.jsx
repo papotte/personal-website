60f69a68fa25b23f301c7cfd3daca3c65e03fcf3
Rossana Bermudez <rossana.bermudez.de.la.hoz@pwc.com>
2020-05-11 23:40:28 +0200

    Fixed ScrollToTop

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,18 @@
-import {Layout, ScrollToTop, SEO} from 'components/common'
+import {Layout, SEO} from 'components/common'
 import {Contact, Experience, Intro, Skills} from 'components/landing'
 import React from 'react'
+import ScrollToTop from 'react-scroll-up'
 import 'styles/main.scss'
 
 export default () => (
   <Layout>
-    <ScrollToTop />
+    <ScrollToTop showUnder={160}>
+      <div className="button is-fab is-energized">
+        <span className="icon" style={{marginTop: -3 + 'px'}}>
+          <i className="fi icon-chevron-up" />
+        </span>
+      </div>
+    </ScrollToTop>
     <SEO />
     <Intro />
     <Skills />
