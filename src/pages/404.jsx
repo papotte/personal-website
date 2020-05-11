ad4f50ffb4bab2583d5c8c2fec6b2a9ba70368a3
Rossana Bermudez <rossana.bermudez.de.la.hoz@pwc.com>
2020-05-11 17:14:54 +0200

    Fixed not-found site

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,22 @@
+import illustration from 'assets/illustrations/not_found.svg'
+import {Container} from 'components/common/Container'
 import {Layout} from 'components/common/Layout'
+import {Details, IntroWrapper} from 'components/landing/Intro/styles'
 import React from 'react'
 
 export default () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container">
+      <IntroWrapper as={Container}>
+        <Details>
+          <h1>NOT FOUND</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          <a href="../" className="button is-primary">Go back</a>
+        </Details>
+        <figure className="image">
+          <img src={illustration} alt="not found" style={{width: 450 + 'px'}} />
+        </figure>
+      </IntroWrapper>
+    </div>
   </Layout>
 )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import {Layout, SEO} from 'components/common'
+import {Layout, ScrollToTop, SEO} from 'components/common'
 import {Contact, Experience, Intro, Skills} from 'components/landing'
 import React from 'react'
 import 'styles/main.scss'
@@ -6,18 +6,21 @@ import 'styles/main.scss'
 let fabButton = document.getElementById('to-top')
 window.onscroll = () => {
   // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
-  if(fabButton.classList) {
+  if (fabButton) {
     let y = window.scrollY
     if (y > 0) {
       fabButton.classList.remove('hidden')
     } else {
       fabButton.classList.add('hidden')
     }
+  } else {
+    fabButton = document.getElementById('to-top')
   }
 }
 
 export default () => (
   <Layout>
+    <ScrollToTop />
     <SEO />
     <Intro />
     <Skills />
