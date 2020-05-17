b2ae8fe1bcf1fb3ae01d10c42e1ab75e8748c4ab
Rossana Bermudez <rossana.bermudez.de.la.hoz@pwc.com>
2020-05-17 13:49:02 +0200

    Randomize intro image

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -1,4 +1,6 @@
-import dev from 'assets/illustrations/simple-dev.svg'
+import dev1 from 'assets/illustrations/dev_cat.svg'
+import dev2 from 'assets/illustrations/dev_js.svg'
+import dev3 from 'assets/illustrations/dev_phone.svg'
 import {Container} from 'components/common'
 import {Header} from 'components/theme'
 import Data from 'data/config'
@@ -6,6 +8,18 @@ import {FormattedMessage} from 'gatsby-plugin-intl'
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import {Details, IntroWrapper, Wrapper} from './styles'
+
+function randomImg() {
+  let number = Math.floor(Math.random() * 3)
+  switch (number) {
+    case 1:
+      return dev2
+    case 2:
+      return dev3
+    default:
+      return dev1
+  }
+}
 
 export const Intro = () => (
   <Wrapper id="about">
@@ -18,8 +32,8 @@ export const Intro = () => (
           <FormattedMessage id={'buttons.contact'} />
         </AnchorLink>
       </Details>
-      <figure className="image">
-        <img src={dev} alt={Data.defaultDescription} />
+      <figure className="image" style={{width: 40 + '%'}}>
+        <img src={randomImg()} alt={Data.defaultDescription} />
       </figure>
     </IntroWrapper>
   </Wrapper>
